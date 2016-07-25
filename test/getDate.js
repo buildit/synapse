96031c028eaf218618fddca21933d9fd19978121
Zachary Smith <william.smith1@wipro.com>
2016-07-25 10:49:03 +0100

    Fix getDate test to align with proper calculation of UTC format.

--- test/getDate.js
+++ test/getDate.js
@@ -8,3 +8,30 @@ describe('Function that converts sting yyyy-mm-dd to UTC numeric value', () => {
     should.not.equal(march, april);
   });
 });
+
+describe('Date getter', () => {
+  const date = '2016-05-09';
+
+  it('should return the day', () => {
+    const day = getDate.day(date);
+    should.equal(day, '09');
+  });
+
+  it('should return the month', () => {
+    const month = getDate.month(date);
+    should.equal(month, '05');
+  });
+
+  it('should return the year', () => {
+    const year = getDate.year(date);
+    should.equal(year, '2016');
+  });
+
+  it('should return UTC format', () => {
+    const utc = getDate.utc(date);
+    should.equal(utc, 1462752000000);
+
+    const beginningOfTimeUTC = getDate.utc('1970-01-01');
+    should.equal(beginningOfTimeUTC, 0);
+  });
+});
