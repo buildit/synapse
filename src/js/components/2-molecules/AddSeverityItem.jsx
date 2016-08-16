15e7847d0b62ea0c33d67aead81ea568e932a6f6
Zachary Smith <william.smith1@wipro.com>
2016-08-16 15:27:07 -0400

    Do not allow "sequence" name values to be blank
    Button is disabled until the "Name" field is filled in. Right now, we're just checking for the presence of any value. We could easily make the validation more sophisticated, such as checking for three or more characters, or whatever logic we like.
    
    I switched to handling the state of this miniature form using the component's state instead of ref. To do so, I switched to using the class syntax instead of the pure function deal.

--- src/js/components/2-molecules/AddSeverityItem.jsx
+++ src/js/components/2-molecules/AddSeverityItem.jsx
@@ -1,35 +1,68 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import Button from '../1-atoms/Button';
 
-const AddSeverityItem = ({ onAddClick }) => {
-  let nameInput;
-  let groupWithInput;
-  return (
-    <div className="add-severity-item">
-      <span>Name</span>
-      <input
-        ref={node => {
-          nameInput = node;
-        }}
-      />
-      <span>Group With</span>
-      <input
-        ref={node => {
-          groupWithInput = node;
-        }}
-      />
-      <Button
-        label="Add"
-        onClick={event => {
-          event.preventDefault();
-          onAddClick(nameInput.value, groupWithInput.value);
-          nameInput.value = '';
-          groupWithInput.value = '';
-        }}
-      />
-    </div>
-  );
-};
+class AddSeverityItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nameValue: '',
+      groupWithValue: '',
+      isValid: false,
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleGroupWithChange = this.handleGroupWithChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(event) {
+    event.preventDefault();
+    this.props.onAddClick(this.state.nameValue, this.state.groupWithValue);
+    this.setState({
+      nameValue: '',
+      groupWithValue: '',
+      isValid: false,
+    });
+  }
+
+  handleNameChange(event) {
+    this.setState({
+      nameValue: event.target.value,
+      isValid: this.isValid(event.target.value),
+    });
+  }
+
+  handleGroupWithChange(event) {
+    this.setState({
+      groupWithValue: event.target.value,
+    });
+  }
+
+  isValid(value) {
+    return value !== '';
+  }
+
+  render() {
+    return (
+      <div className="add-severity-item">
+        <span>Name</span>
+        <input
+          value={this.state.nameValue}
+          onChange={this.handleNameChange}
+        />
+        <span>Group With</span>
+        <input
+          value={this.state.groupWithValue}
+          onChange={this.handleGroupWithChange}
+        />
+        <Button
+          label="Add"
+          onClick={this.onClick}
+          disabled={!this.state.isValid}
+        />
+      </div>
+    );
+  }
+}
 
 export default AddSeverityItem;
 
