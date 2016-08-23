2d4bd5c1f66e293901ebf9df8a7af87c1a824753
Jorge Sanchez <jorge.sanchez@wipro.com>
2016-08-22 20:22:58 -0400

    scaling is working but there is a an issue with the y values not showing up.

--- src/js/components/2-molecules/StatusChart.jsx
+++ src/js/components/2-molecules/StatusChart.jsx
@@ -6,6 +6,7 @@ export default class StatusChart extends React.Component {
     super();
     this.points = null;
     this.chart = null;
+    this.chartEffort = null;
     this.vis = null;
     this.parseTime = d3.timeParse('%d-%b-%y');
     this.yOffset = {
@@ -57,21 +58,22 @@ export default class StatusChart extends React.Component {
   getScale(yOffset = 0, data = [], categories = []) {
     const size = this.getSize();
     const dates = data.map(datapoint => this.parseTime(datapoint.date));
-    // const maxYs = data.reduce((maxes, dataPoint) => {
-    //   const dataPointMax = categories.reduce((sumAtPoint, category) => (
-    //     sumAtPoint + dataPoint[category]
-    //   ), 0);
-    //   return maxes.concat(dataPointMax);
-    // }, []);
-    //
-    // const maxY = maxYs.reduce((max, current) => (current > max ? current : max), 0);
-    let maxY = 200;
-    if (this.props.projectId === 'P001') {
-      maxY = 500;
-    }
-    if (this.props.projectId === 'P003') {
-      maxY = 140;
-    }
+    const maxYs = data.reduce((maxes, dataPoint) => {
+      const dataPointMax = categories.reduce((sumAtPoint, category) => (
+        sumAtPoint + dataPoint[category]
+      ), 0);
+      return maxes.concat(dataPointMax);
+    }, []);
+
+    const maxY = maxYs.reduce((max, current) => (current > max ? current : max), 0);
+    // console.log("maxy",maxY)
+    // let maxY = 10;
+    // if (this.props.projectId === 'P001') {
+    //   maxY = 500;
+    // }
+    // if (this.props.projectId === 'P003') {
+    //   maxY = 140;
+    // }
     return {
       date: d3.scaleTime()
         .domain(d3.extent(dates))
@@ -128,31 +130,34 @@ export default class StatusChart extends React.Component {
     d3.select('#demand').remove();
     d3.select('#defect').remove();
     d3.select('#effort').remove();
-    this.updateCurve(
+
+    this.updateArea(
       this.yOffset.demand,
       this.props.data,
       this.props.demandCategories
     );
 
-    this.updateCurve(
+    this.updateArea(
       this.yOffset.defect,
       this.props.defectStatus,
       this.props.defectCategories
     );
 
-    this.updateCurveLine(
+    this.updateArea(
       this.yOffset.effort,
       this.props.effortStatus,
       this.props.effortCategories
     );
   }
 
-  updateCurve(yOffset, data = [], categories) {
+
+  updateArea(yOffset, data = [], categories) {
     const parseTime = this.parseTime;
     const xScale = this.getScale(yOffset, data, categories).date;
     const yScale = this.getScale(yOffset, data, categories).y;
 
     const area = d3.area()
+      .curve(d3.curveCardinal)
       .x(d => xScale(parseTime(d.data.date)))
       .y0(d => yScale(d[0] || 0))
       .y1(d => yScale(d[1] || 0));
@@ -201,36 +206,91 @@ export default class StatusChart extends React.Component {
         .text(d => d.key);
   }
 
-  updateCurveLine(yOffset, data = [], categories) {
+  updateAreaEffort(yOffset, data = [], categories) {
+    const parseTime = this.parseTime;
+    const xScale = this.getScale(yOffset, data, categories).date;
+    const yScale = this.getScale(yOffset, data, categories).y;
+
+    const area = d3.area()
+      .curve(d3.curveCardinal)
+      .x(d => xScale(parseTime(d.data.date)))
+      .y0(d => yScale(d[0] || 0))
+      .y1(d => yScale(d[1] || 0));
+
+    const stack = d3.stack()
+      .keys(categories)
+      .order(d3.stackOrderReverse)
+      .offset(d3.stackOffsetNone);
+
+    if (data.length > 0) {
+      const stackContainer = this.vis.append('g')
+        .attr('class', 'stack');
+
+      const layer = stackContainer.selectAll('.layer')
+        .data(stack(data))
+        .enter()
+        .append('g')
+        .attr('class', 'layer');
+
+      layer.append('path')
+        .attr('class', 'area')
+        .attr('stroke', 1)
+        .style('fill', '(d, i) => d3.schemeCategory20[i]')
+        .attr('d', area);
+    }
+
+    const legend = this.vis.append('g')
+      .attr('class', 'legend');
+
+    legend.selectAll('.legend-item')
+      .data(stack(data))
+      .enter()
+      .append('circle')
+        .attr('r', 5)
+        .attr('cx', 20)
+        .attr('cy', (d, i) => yOffset + 20 + i * 12)
+        .attr('stroke', 'none')
+        .attr('fill', (d, i) => d3.schemeCategory20[i]);
+
+    legend.selectAll('.legend-item')
+      .data(stack(data))
+      .enter()
+      .append('text')
+        .attr('class', 'legend-item')
+        .attr('x', 30)
+        .attr('y', (d, i) => yOffset + 24 + i * 12)
+        .text(d => d.key);
+  }
+
+  updateLine(yOffset, data = [], categories) {
     const dates = data.map(item => (item.date));
-    const values = data.map(item => (item.value));
-    console.log("values", values);
 
-    const margin = { top: 20, right: 20, bottom: 40, left: 50 };
-    const width = 600 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+    // for each category
+    const chartableArrays = categories.map(category => {
+      const chartableData = dates.map((date, i) => {
+        let value = 0;
+        if (data[i][category]) {
+          value = data[i][category];
+        }
+        return ({
+          category,
+          date,
+          value,
+        });
+      });
+      return chartableData;
+    });
 
-    const xScale = d3.scaleTime()
-        .domain(d3.extent(dates))
-        .range([0, width]);
-
-    const yScale = d3.scaleLinear()
-        .domain(d3.extent(values))
-        .range([height, 0]);
+    const xScale = this.getScale(yOffset, data, categories).date;
+    const yScale = this.getScale(yOffset, data, categories).y;
 
     const svg =
-        d3.select(this.chart)
+        d3.select(this.chartEffort)
           .append('svg')
-          .attr('width', width + margin.left + margin.right)
-          .attr('height', height + margin.top + margin.bottom)
           .append('g')
-          .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+          .attr('id', 'line')
+          .attr('transform', 'translate(10, 10)');
 
-      /* x axis */
-    const chart1 = svg.append('g')
-        .attr('class', 'axis axis--x')
-        .attr('transform', 'translate(0, ' + (height + 10) + ')')
-        .call(d3.axisBottom(xScale));
 
       /* y axis */
     svg.append('g')
@@ -239,49 +299,23 @@ export default class StatusChart extends React.Component {
         .call(d3.axisLeft(yScale));
 
     const line = d3.line()
-        .curve(d3.curveCardinal.tension(0.1))
-        .x(function(d) {
-          return xScale(d.date);
-        })
-        .y(function(d) {
-          return yScale(d.value);
-        });
+    .curve(d3.curveCardinal.tension(0.1))
+    .x(function(d) {
+      return xScale(d.date)
+    })
+    .y(function(d) {
+      return yScale(d.value)
+    });
 
-    console.log('data:', data);
     svg.append('path')
-       .datum(data)
+       .datum(chartableArrays[2])
        .attr('class', 'line')
        .attr('d', line);
-
-    // const parseTime = this.parseTime;
-    // const xScale = this.getScale(yOffset, data, categories).date;
-    // const yScale = this.getScale(yOffset, data, categories).y;
-
-    // const area = d3.area()
-    //   .x(d => xScale(parseTime(d.data.date)))
-    //   .y0(d => yScale(d[0] || 0))
-    //   .y1(d => yScale(d[1] || 0));
 
     const stack = d3.stack()
       .keys(categories)
       .order(d3.stackOrderReverse)
       .offset(d3.stackOffsetNone);
-
-    // if (data.length > 0) {
-    //   const stackContainer = this.vis.append('g')
-    //     .attr('class', 'stack');
-    //
-    //   const layer = stackContainer.selectAll('.layer')
-    //     .data(stack(data))
-    //     .enter()
-    //     .append('g')
-    //     .attr('class', 'layer');
-    //
-    //   layer.append('path')
-    //     .attr('class', 'area')
-    //     .style('fill', (d, i) => d3.schemeCategory20[i])
-    //     .attr('d', area);
-    // }
 
     const legend = this.vis.append('g')
       .attr('class', 'legend');
@@ -312,6 +346,10 @@ export default class StatusChart extends React.Component {
         <div
           className="chart-container"
           ref={(c) => { this.chart = c; return false; }}
+        />
+        <div
+          className="chart-effort"
+          ref={(c) => { this.chartEffort = c; return false; }}
         />
       </div>
     );
