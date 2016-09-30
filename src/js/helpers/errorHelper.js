12f3409bc3b0954138a2e4f1f01aaadb6ce68190
Zachary Smith <william.smith1@wipro.com>
2016-09-30 15:30:32 -0400

    Handle fetch failure cases more elegantly

--- src/js/helpers/errorHelper.js
+++ src/js/helpers/errorHelper.js
@@ -1,16 +1,23 @@
-// Takes a response from the api and returns a string that represents the error.
+// Takes a thing from the api and returns a string that represents the error.
+// TODO: Clarify the expected cases,
+// and use meaningful variable names that make these cases apparent.
 
-const errorHelper = response => {
-  if (response && response.body) {
-    return response.body;
+const errorHelper = thing => {
+  if (thing && thing.body) {
+    const innerThing = JSON.parse(thing.body);
+    if (innerThing.error.message) return innerThing.error.message;
+    if (innerThing.error.statusCode) return innerThing.error.statusCode;
   }
 
-  if (response && response.statusText) {
-    return response.statusText;
+  if (thing && thing.statusText) {
+    return thing.statusText;
   }
 
-  if (response && response.message) {
-    return response.message;
+  if (thing && thing.message) {
+    if (thing.message === 'Internal XMLHttpRequest Error') {
+      return 'We are unable to reach the server at this time.';
+    }
+    return thing.message;
   }
 
   return 'An unknown error occurred.';
