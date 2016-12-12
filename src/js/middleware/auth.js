c96de8bef9451a40427f28c736d0f7e7dc0b402b
Zachary Smith <william.smith1@wipro.com>
2016-12-12 16:46:45 -0700

    SYNAPSE-89 #comment Refresh no longer logs out user
    If the user has already logged in, they get logged in automatically, even if they've shut down the browser in the meantime. The app is doing a simple check for the presence of a "user" in local storage. So this is really not secure! But I think it gets us to a "good enough" sort of auth for now.

--- src/js/middleware/auth.js
+++ src/js/middleware/auth.js
@@ -1,27 +1,23 @@
 import { takeEvery } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
-import Cookies from 'js-cookie';
 
 import { postLoginRequest } from 'middleware/api';
 
 import { LOGIN_REQUEST, LOGOUT_REQUEST } from 'actions/actions';
 import { loginSuccess, loginFailure, logoutSuccess, setMessage } from 'actions';
 
-const getCookie = response => {
-  if (response.user && response.user.id) return response.user.id;
+const getUser = response => {
+  if (response.user && response.user.id) return { name: response.user.id };
   return false;
 };
 
 export function* loginRequest(action) {
   try {
     const response = yield call(postLoginRequest, action.user);
-    const cookie = getCookie(response);
-    if (cookie) {
-      Cookies.set('user', cookie);
-      const user = {
-        name: action.user.email,
-      };
-      yield put(loginSuccess(user, cookie));
+    const user = getUser(response);
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+      yield put(loginSuccess(user));
       yield put(setMessage(`Welcome, ${user.name}`));
     } else {
       yield put(loginFailure());
@@ -35,7 +31,7 @@ export function* watchLoginRequest() {
 }
 
 export function* logoutRequest() {
-  Cookies.remove('user');
+  localStorage.removeItem('user');
   yield put(logoutSuccess());
   yield put(setMessage('You are logged out'));
 }
