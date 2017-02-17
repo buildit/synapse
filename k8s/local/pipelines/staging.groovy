721a3f8c643e25925b74b54786116e8a489e2636
rsafronov <electroma@gmail.com>
2017-02-16 21:26:35 -0500

    Migrated pipeline to k8s helpers

--- k8s/local/pipelines/staging.groovy
+++ k8s/local/pipelines/staging.groovy
@@ -1,119 +1,102 @@
 @Library('buildit')
-def LOADED = true
-podTemplate(label: 'nodeapp',
-  containers: [
-    containerTemplate(name: 'nodejs-builder', image: 'builditdigital/node-builder', ttyEnabled: true, command: 'cat', privileged: true),
-    containerTemplate(name: 'docker', image: 'docker:1.11', ttyEnabled: true, command: 'cat'),
-    containerTemplate(name: 'kubectl', image: 'builditdigital/kube-utils', ttyEnabled: true, command: 'cat')],
-  volumes: [
-    hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
-    hostPathVolume(mountPath: '/var/cache', hostPath: '/tmp'),
-    hostPathVolume(mountPath: '/var/projects', hostPath: '/Users/romansafronov/dev/projects')
-  ]) {
-  node('nodeapp') {
+import buildit.*
 
-    currentBuild.result = "SUCCESS"
-    sendNotifications = false //FIXME !DEV_MODE
+gitInst = new git()
+slackInst = new slack()
+k8s = new K8S(this, Cloud.local)
 
-    try {
-      stage('Set Up') {
-        gitInst = new git()
+sendNotifications = false //FIXME !DEV_MODE
+buildNumber = env.BUILD_NUMBER
+appName = "synapse"
+targetEnv = "staging"
+slackChannel = "synapse"
+gitUrl = "https://github.com/buildit/synapse.git"
+dockerRegistry = "builditdigital"
+image = "$dockerRegistry/$appName"
+appUrl = 'http://synapse.stage.kube.local'
+
+k8s.build([containerTemplate(name: 'nodejs-builder', image: 'builditdigital/node-builder', ttyEnabled: true, command: 'cat', privileged: true)],
+  [hostPathVolume(mountPath: '/var/projects', hostPath: '/Users/romansafronov/dev/projects')]) {
+
+  try {
+    container('nodejs-builder') {
+      stage('Checkout') {
+        //checkout scm
+        git(url: '/var/projects/synapse', branch: 'spike/security_perimeter')
+
+        shortCommitHash = gitInst.getShortCommit()
+        commitMessage = gitInst.getCommitMessage()
         npmInst = new npm()
-        slackInst = new slack()
-
-        buildNumber = env.BUILD_NUMBER
-        appName = "synapse"
-        cloud = "local"
-        env = "staging"
-        slackChannel = "synapse"
-        gitUrl = "https://github.com/buildit/synapse.git"
-        dockerRegistry = "builditdigital"
-        image = "$dockerRegistry/$appName"
-        deployment = "${appName}-${env}"
+        version = npmInst.getVersion()
       }
-      container('nodejs-builder') {
-        stage('Checkout') {
-          checkout scm
-          //git(url: '/var/projects/synapse', branch: 'spike/security_perimeter')
 
-          shortCommitHash = gitInst.getShortCommit()
-          commitMessage = gitInst.getCommitMessage()
-          version = npmInst.getVersion()
-        }
-
-        stage("Install") {
-          // poor man's caching for node modules
-          sh 'mkdir -p /var/cache/synapse-build/_cache'
-          sh 'cp -r /var/cache/synapse-build/* .'
+      stage("Install") {
+        k8s.withCache('node_modules') {
           sh "npm install"
-          sh "rm -rf /var/cache/synapse-build/* && cp -r node_modules /var/cache/synapse-build/"
-        }
-
-        stage("Test") {
-          try {
-            sh "npm run test:ci"
-          }
-          finally {
-            junit 'reports/test-results.xml'
-          }
-        }
-
-        stage("Analysis") {
-          sh "npm run lint"
-        }
-
-        stage("Build") {
-          sh "NODE_ENV='removeme' npm run build"
-        }
-      }
-      container('docker') {
-        stage('Docker Image Build') {
-          tag = "${version}-${shortCommitHash}-${buildNumber}"
-          // Docker pipeline plugin does not work with kubernetes (see https://issues.jenkins-ci.org/browse/JENKINS-39664)
-          sh "docker build -t $image:$tag ."
-          //ecrInst.authenticate(env.AWS_REGION) FIXME
         }
       }
 
-      container('kubectl') {
-        stage('Deploy To K8S') {
-          def deployment = "$appName-$env"
-          deploymentObj = "$deployment-$appName".take(24)
-          def varsFile = "./k8s/${cloud}/vars/${env}.yaml"
-          sh "helm ls -q | grep $deployment || helm install ./k8s/synapse -f $varsFile -n $deployment"
-          sh "helm upgrade $deployment ./k8s/synapse -f $varsFile --set image.repository=$image --set image.tag=$tag"
-          sh "kubectl rollout status deployment/$deploymentObj"
+      stage("Test") {
+        try {
+          sh "npm run test:ci"
+        }
+        finally {
+          junit 'reports/test-results.xml'
         }
       }
 
+      stage("Analysis") {
+        sh "npm run lint"
+      }
+
+      stage("Build") {
+        sh "NODE_ENV='removeme' npm run build"
+      }
+    }
+
+    stage('Docker Image Build') {
+      tag = "${version}-${shortCommitHash}-${buildNumber}"
+      k8s.dockerBuild(image, tag)
+    }
+
+    stage('Docker Push') {
+      k8s.dockerPush(image, tag)
+    }
+
+    stage('Deploy To K8S') {
+      k8s.helmDeploy(appName, targetEnv, image, tag)
+    }
+
+    stage('Run Functional Acceptance Tests') {
       container('nodejs-builder') {
-        stage('Run Functional Acceptance Tests') {
-          //nasty workaround for temporary chrome socket issue (can't use remote mount for it)
-          sh "mkdir /tmp/wscopy && cd . && ls -1 | xargs -I '{}'  ln -s `pwd`/{} /tmp/wscopy/{}"
 
-          try {
-            // nasty workaround for local env (in case you haven't installed dnsmasq)
-            sh "echo '192.168.99.100 synapse.kube.local synapse.kube.local heimdall.kube.local' > /etc/hosts"
-            sh "cd /tmp/wscopy && URL=http://synapse.kube.local xvfb-run -s '-screen 0 1280x1024x16' npm run test:acceptance:ci"
-          }
-          finally {
-            archiveArtifacts allowEmptyArchive: true, artifacts: 'screenshots/*.png'
-            junit 'reports/acceptance-test-results.xml'
-          }
+        //nasty workaround for temporary chrome socket issue (can't use remote mount for it)
+        sh "mkdir /tmp/wscopy && cd . && ls -1 | xargs -I '{}'  ln -s `pwd`/{} /tmp/wscopy/{}"
+
+        try {
+          // nasty workaround for local env (in case you haven't installed dnsmasq)
+          sh "echo '192.168.99.100 synapse.stage.kube.local synapse.stage.kube.local heimdall.stage.kube.local' > /etc/hosts"
+          sh "cd /tmp/wscopy && URL=${appUrl} xvfb-run -s '-screen 0 1280x1024x16' npm run test:acceptance:ci"
         }
-      }
-
-      container('docker') {
-        stage('Promote Build to latest') {
-          sh "docker tag $image:$tag $image:latest"
-          if (sendNotifications) slackInst.notify("Deployed to Staging", "Commit <${gitUrl}/commits/${shortCommitHash}|${shortCommitHash}> has been deployed to ${env}\n\n${commitMessage}", "good", "http://i3.kym-cdn.com/entries/icons/square/000/002/230/42.png", slackChannel)
+        finally {
+          archiveArtifacts allowEmptyArchive: true, artifacts: 'screenshots/*.png'
+          junit 'reports/acceptance-test-results.xml'
         }
       }
     }
-    catch (err) {
-      currentBuild.result = "FAILURE"
-      if (sendNotifications) slackInst.notify("Error while deploying to Staging", "Commit <${gitUrl}/commits/${shortCommitHash}|${shortCommitHash}> failed to deploy to ${env}", "danger", "http://i2.kym-cdn.com/entries/icons/original/000/002/325/Evil.jpg", slackChannel)
-      throw err
+
+    stage('Promote Build to latest') {
+      k8s.inDocker {
+        sh "docker tag $image:$tag $image:latest"
+      }
+      k8s.dockerPush(image, 'latest')
+      if (sendNotifications) slackInst.notify("Deployed to Staging", "Commit <${gitUrl}/commits/${shortCommitHash}|${shortCommitHash}> has been deployed to ${targetEnv}\n\n${commitMessage}", "good", "http://i3.kym-cdn.com/entries/icons/square/000/002/230/42.png", slackChannel)
     }
+
+  }
+  catch (err) {
+    currentBuild.result = "FAILURE"
+    if (sendNotifications) slackInst.notify("Error while deploying to Staging", "Commit <${gitUrl}/commits/${shortCommitHash}|${shortCommitHash}> failed to deploy to ${targetEnv}", "danger", "http://i2.kym-cdn.com/entries/icons/original/000/002/325/Evil.jpg", slackChannel)
+    throw err
   }
 }
